d34dbbb05b16286e39c184a854c41aae43dd36de
chisatonishikigi479 <randomaopstroller3@gmail.com>
2025-10-25 09:45:43 -0500

    awrgawrgawrg

---
+++ bedtime_slider_container.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+
+func _on_bedtime_slider_value_changed(value: float) -> void:
+	$BedtimeLabel.text = GlobalData.minutes_to_time_string(int(value))
+	GlobalData.bedtime = value
+	
+	print(GlobalData.bedtime)
+	pass # Replace with function body.
--- global_data.gd
+++ global_data.gd
@@ -14,5 +14,18 @@ var prompt_responses_during_day: Array = [] #array of strings
 func setup():
 	prompt_responses_during_day.append("") #0-index prompt is automatically empty bc the app doesn't ask how you're feeling on the title screen
 	
+func minutes_to_time_string(minutes: float) -> String:
+	var clamped_minutes = int(minutes) % 1440
+	var hours = clamped_minutes / 60
+	var mins = clamped_minutes % 60
+	var period = "AM"
+	var display_hours = hours
+	if hours >= 12:
+		period = "PM"
+		if hours > 12:
+			display_hours = hours - 12
+	if hours == 0:
+		display_hours = 12  
+	return "%d:%02d %s" % [display_hours, mins, period]
 
 	
